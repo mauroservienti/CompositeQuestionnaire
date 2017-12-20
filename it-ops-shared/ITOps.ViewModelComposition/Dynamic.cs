1d22592fb1cc22e4b393b46501d8a62e2c09982a
Mauro Servienti <mauro.servienti@gmail.com>
2017-12-20 19:09:16 +0100

    Add support to detect properties existance on dynamics

---
+++ it-ops-shared/ITOps.ViewModelComposition/Dynamic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace ITOps.ViewModelComposition
+{
+    public static class Dynamic
+    {
+        public static bool HasOwnProperty(dynamic settings, string name)
+        {
+            if (settings is ExpandoObject)
+            {
+                return ((IDictionary<string, object>)settings).ContainsKey(name);
+            }
+
+            return settings.GetType().GetProperty(name) != null;
+        }
+    }
+}
