abd583f5f28d0561202a6bfb48006f832e710827
Mauro Servienti <mauro.servienti@gmail.com>
2017-12-20 19:18:13 +0100

    Add Rules composition

---
+++ Rules.ViewModelComposition/QuestionRulesPutHandler.cs
@@ -0,0 +1,59 @@
+﻿using ITOps.Json;
+using ITOps.ViewModelComposition;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rules.ViewModelComposition
+{
+    public class QuestionRulesPutHandler : IHandleRequests, IHandleRequestsErrors
+    {
+        public bool Matches(RouteData routeData, string httpVerb, HttpRequest request)
+        {
+            var controller = (string)routeData.Values["controller"];
+            var action = (string)routeData.Values["action"];
+
+            return HttpMethods.IsPut(httpVerb)
+                   && controller.ToLowerInvariant() == "questions"
+                   && string.IsNullOrWhiteSpace(action)
+                   && !routeData.Values.ContainsKey("id");
+        }
+
+        public async Task Handle(string requestId, dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            dynamic putData = request.Body.AsExpando();
+            var rules = new List<dynamic>()
+            {
+                new
+                {
+                    putData.QuestionId,
+                    putData.CorrectAnswerId
+                }
+            };
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Put, $"http://localhost:54219/api/rules")
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(rules), Encoding.UTF8, "application/json")
+            };
+            requestMessage.Headers.Add("composed-request-id", requestId);
+
+            var response = await new HttpClient()
+                .SendAsync(requestMessage);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new InvalidOperationException(response.ReasonPhrase);
+            }
+        }
+
+        public Task OnRequestError(string requestId, Exception ex, dynamic vm, RouteData routeData, HttpRequest request)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
