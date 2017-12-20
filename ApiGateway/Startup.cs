0c899df75774a74f1797988f9f11fd053d1f00d3
Mauro Servienti <mauro.servienti@gmail.com>
2017-12-20 19:18:28 +0100

    Add composition API gateway

---
+++ ApiGateway/Startup.cs
@@ -0,0 +1,51 @@
+﻿using ITOps.ViewModelComposition;
+using ITOps.ViewModelComposition.Gateway;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace ApiGateway
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+            services.AddViewModelComposition();
+            services.AddCors();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            app.Use((context, next) =>
+            {
+                context.Request.EnableRewind();
+                return next();
+            });
+
+            app.UseCors(policyBuilder =>
+            {
+                policyBuilder.AllowAnyOrigin();
+                policyBuilder.AllowAnyMethod();
+                policyBuilder.AllowAnyHeader();
+            });
+
+            app.RunCompositionGateway(routes =>
+            {
+                routes.MapComposableGet(template: "{controller}/{id:guid?}");
+                routes.MapComposablePost(template: "{controller}/{id:guid}");
+                routes.MapComposablePut(template: "{controller}");
+                routes.MapComposableDelete(template: "{controller}/{id:guid}");
+                routes.MapRoute("{*NotFound}", context =>
+                {
+                    context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    return Task.CompletedTask;
+                });
+            });
+        }
+    }
+}
